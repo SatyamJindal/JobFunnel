46be5a4a188b61e94ad7357026f9f4e2fd16c283
riyaagrahari <500052961@stu.upes.ac.in>
2020-02-18 10:51:16 +0530

    Filtering Improvememnts

--- jobfunnel/glassdoor.py
+++ jobfunnel/glassdoor.py
@@ -248,9 +248,17 @@ class GlassDoor(JobFunnel):
                 #print(job['company'].lower())
                 search_key = str(self.search_terms['keywords'][0]).lower().strip('developer').strip()
                 first_job = job['company'].lower().strip()
+          
+                flag = 0
+                search_list = search_key.split()
+                for i in search_list:
+                    if i in first_job:
+                        flag = 1
+                        break 
+                        
                 #print('fir',search_key,'sec',first_job)
                 #print(str(search_key).lower().strip()==str(first_job).lower().strip())
-                if(search_key!=first_job):
+                if(flag == 0):
                     log_info('No searches found on glassdoor')
                     status = 1
                     continue
